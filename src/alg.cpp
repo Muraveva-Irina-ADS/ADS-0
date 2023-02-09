695429d51075d9ab8a1f4b10fafc20d8ca131101
Muraveva-Irina-ADS <124904325+Muraveva-Irina-ADS@users.noreply.github.com>
2023-02-09 21:26:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,6 @@
 // Copyright 2021 NNTU-CS
-
 int gcd(int a, int b) {
-    int nod;
+        int nod;
 	int max = a>b ? a : b;
 	for (int i = 1; i <= max; i++)
 		if ((a % i == 0) && (b % i == 0)) nod = i;
